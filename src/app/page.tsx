af3610fc5368c5edf5876cef559374fa483212e3
Kalleo Pinheiro <windson.rocha@zup.com.br>
2023-05-08 14:42:23 -0300

    feat: :sparkles: update Home page title to "Hello World Test 2"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between lg:flex'>
         <div className='relative flex place-items-center'>
-          <p className={`${inter.className} text-2xl text-cyan-950`}>Hello World Test 🚀 </p>
+          <p className={`${inter.className} text-2xl text-cyan-950`}>Hello World Test 2 🚀 </p>
         </div>
       </div>
     </main>
